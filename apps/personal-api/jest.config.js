e6ee05531f6da727d2f2a06297b40434fb542e72
Abdul Salam <abdulsalam121196@gmail.com>
2021-01-31 21:20:15 +0700

    initial commit

---
+++ apps/personal-api/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'personal-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/personal-api',
+};
